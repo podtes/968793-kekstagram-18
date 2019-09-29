482271a5fa0b02dcda03d4995da476e860f884c4
podtes <dpodtes@gmail.com>
2019-09-29 20:06:25 +0300

    исправил замечания по личному проекту

--- js/main.js
+++ js/main.js
@@ -1,213 +1,98 @@
 'use strict';
 
+var AUTHOR_NAMES = ['Алексей', 'Дмитрий', 'Василий', 'Юрий', 'Бенджамин', 'Георгий', 'Джон', 'Юлия', 'Мария', 'Валерия', 'Валерий', 'Евгений', 'Константин', 'Анна', 'Эдуард'];
+var AUTHOR_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var AUTHOR_DESCRIPTIONS = ['Классно посидели!', 'Вот такой отпуск', 'Вы только посмотрите на это!', 'Какая то умная цитата несуществующего философа', 'Размышления о жизни', 'Сходил на бизнес-тренинг и теперь могу постить такие фотографии', 'Завидуйте мне', 'Просто фотография еды'];
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+var photos = [];
+
 /**
- * Функция вычисляет случайное число из интервала от минимального до максимального, включая эти значения
- * @param {number} min минимальное значение интервала
- * @param {number} max максимальное значение интервала
+ * @param {number} min
+ * @param {number} max
  * @return {number} случайное число из интервала
  */
-var randomIntFromInterval = function (min, max) {
+var getRandomIntFromInterval = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 /**
- * Функция функция возвращает случайный элемент массива
- * @param {string} arrayName название массива, из которого будем выбирать слкчайный элемент
- * @return {number[]} randomValue случайный элемент массива
+ * Функция возвращает случайный элемент массива
+ * @param {*[]} arr массив, из которого будем выбирать случайный элемент
+ * @return {*} randomValue случайный элемент массива
  */
-var getRandomValue = function (arrayName) {
-  var randomValue = Math.floor(Math.random() * arrayName.length);
+var getRandomArrayElement = function (arr) {
+  var randomValue = Math.floor(Math.random() * arr.length);
   return randomValue;
 };
 
-var pictureTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture'); // хотелось бы подробнее разобрать эту запись и что по итогу мы записываем в переменную pictureTemplate?
+/**
+ * Функция возвращает первый сгенерированный элемент будущего массива с фотографиями
+ * @param {number} numberFirstPhoto число, с которого начнется генерация адресов, из которых будут загружаться фото и аватарки
+ * @return {{
+ *   url: string,
+ *   description: string,
+ *   likes: number,
+ *   comments: {
+ *     avatar: string,
+ *     message: string,
+ *     name: string
+ *   }
+ * }}
+ */
+var createPhoto = function (numberFirstPhoto) {
+  var photo = {
+    url: './photos/' + numberFirstPhoto + '.jpg',
+    description: AUTHOR_DESCRIPTIONS[getRandomArrayElement(AUTHOR_DESCRIPTIONS)],
+    likes: getRandomIntFromInterval(LIKES_MIN, LIKES_MAX),
+    comments: {
+      avatar: './img/avatar-' + numberFirstPhoto + '.svg',
+      message: AUTHOR_COMMENTS[getRandomArrayElement(AUTHOR_COMMENTS)],
+      name: AUTHOR_NAMES[getRandomArrayElement(AUTHOR_NAMES)]
+    }
+  };
+  return photo;
+};
 
-var AUTHOR_NAMES = ['Алексей', 'Дмитрий', 'Василий', 'Юрий', 'Бенджамин', 'Георгий', 'Юрий', 'Юлия', 'Мария', 'Валерия', 'Валерий', 'Евгений', 'Константин', 'Анна', 'Эдуард'];
-var AUTHOR_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var AUTHOR_DESCRIPTION = ['Классно посидели!', 'Вот такой отпуск', 'Вы только посмотрите на это!', 'Какая то умная цитата несуществующего философа', 'Размышления о жизни', 'Сходил на бизнес-тренинг и теперь могу постить такие фотографии', 'Завидуйте мне', 'Просто фотография еды'];
-
-// можно ли как то генерировать этот массив циклом? Чтобы не писать его руками. Или лучше вот так оставить?
-var photos = [
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
-  },
-  {
-    name: AUTHOR_NAMES[getRandomValue(AUTHOR_NAMES)],
-    comments: AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)] + ' ' + AUTHOR_COMMENTS[getRandomValue(AUTHOR_COMMENTS)],
-    description: AUTHOR_DESCRIPTION[getRandomValue(AUTHOR_DESCRIPTION)],
-    likes: randomIntFromInterval(15, 200)
+/**
+ * Функция создает массив с фотографиями
+ * @param {number} countOfPhotos нужное количетсво элементов массива
+ * @return {[]} вернет массив с элементами
+ */
+var createPhotosArray = function (countOfPhotos) {
+  for (var i = 0; i < countOfPhotos; i++) {
+    photos[i] = createPhoto(i + 1);
   }
-];
-var avatarNumber = 0;
+  return photos;
+};
+
 /**
  * Функция генерирует DOM элемент на основе шаблона pictureTemplate
- * @param {[]} photo массив объектов с данными (моки)
+ * @param {*[]} photo массив объектов с данными (моки)
  * @return {Node} возвращает сгенерированный DOM элемент
  */
 var renderPhotos = function (photo) {
   var pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = './img/avatar-' + avatarNumber + '.svg'; // или адрес изображения тоже нужно было как-то записывать в массив photos?
+  pictureElement.querySelector('.picture__img').src = photo.url;
   pictureElement.querySelector('.picture__likes').textContent = photo.likes;
-  pictureElement.querySelector('.picture__comments').textContent = 2; // так ок?
+  pictureElement.querySelector('.picture__comments').textContent = 1;
 
   return pictureElement;
 };
 
-var fragment = document.createDocumentFragment(); // не могу понять зачем нужно использовать fragment и где он вообще находится?
-
 /**
- * Функция перебирает массив photos и вставляет результат после fragment?
+ * Функция переносит сгенерированные DOM элементы на страницу index.html и вставляет в секцию pictures
  */
-var getPhotosToIndex = function () {
+var putPhotosToPage = function () {
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < photos.length; i++) {
-    avatarNumber++;
     fragment.appendChild(renderPhotos(photos[i]));
   }
+  document.querySelector('.pictures').appendChild(fragment);
 };
 
-getPhotosToIndex();
-
-document.querySelector('.pictures').appendChild(fragment);
+createPhotosArray(25);
+putPhotosToPage();
